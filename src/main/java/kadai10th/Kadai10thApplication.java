d2a6b230ad4b320e949d7c1ff212dd68a94e465d
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2023-01-04 19:55:34 +0900

    Build-up project

---
+++ src/main/java/kadai10th/Kadai10thApplication.java
@@ -0,0 +1,13 @@
+package kadai10th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kadai10thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kadai10thApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kadai10th/Kadai10thApplicationTests.java
@@ -0,0 +1,13 @@
+package kadai10th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kadai10thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
