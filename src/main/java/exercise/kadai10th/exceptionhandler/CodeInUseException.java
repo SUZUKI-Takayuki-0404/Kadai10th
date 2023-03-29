a01d30428d58016ae33f16bd79dbbebfb91f73b8
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2023-03-29 23:15:03 +0900

    Fix incorrect code

--- src/main/java/exercise/kadai10th/exceptionhandler/CodeInUseException.java
+++ src/main/java/exercise/kadai10th/exceptionhandler/CodeInUseException.java
@@ -2,6 +2,6 @@ package exercise.kadai10th.exceptionhandler;
 
 public class CodeInUseException extends RuntimeException {
     public CodeInUseException(String message, Throwable cause) {
-        super(message + " code in use", cause);
+        super(message + " code in use");
     }
 }
