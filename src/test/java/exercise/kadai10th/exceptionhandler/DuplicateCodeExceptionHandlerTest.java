3568f5055471fe31f2bef2a8a6abce1060fb9ce6
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2023-04-15 23:53:27 +0900

    add exceptionhandler test (disabled)

---
+++ src/test/java/exercise/kadai10th/exceptionhandler/DuplicateCodeExceptionHandlerTest.java
@@ -0,0 +1,14 @@
+package exercise.kadai10th.exceptionhandler;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DuplicateCodeExceptionHandlerTest {
+
+    @Test
+    @Disabled
+    void handleDuplicateCodeException() {
+    }
+}
