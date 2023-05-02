5d0b52a72f3d1867e8f612d5e449056567bf6861
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2023-05-02 14:43:35 +0900

    Build IntegrationTest

---
+++ src/test/java/exercise/kadai10th/integration/AirportIntegrationTest.java
@@ -0,0 +1,179 @@
+package exercise.kadai10th.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exercise.kadai10th.controller.AirportController;
+import exercise.kadai10th.entity.AirportEntity;
+import exercise.kadai10th.requestform.AirportRequestForm;
+import exercise.kadai10th.service.AirportService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class AirportIntegrationTest {
+/*
+    @InjectMocks
+    private AirportController airportController;
+
+    @MockBean
+    private AirportService airportService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    private final String BASE_PATH = "json-datasets/";
+
+    private String getJsonFileData(String fileName) throws IOException {
+        var jsonResult = resourceLoader.getResource("classpath:" + BASE_PATH + fileName);
+        return StreamUtils.copyToString(jsonResult.getInputStream(), StandardCharsets.UTF_8);
+    }
+
+    @Test
+    @DisplayName("空港コードから空港データを取得した場合、レスポンスコード200および空港のjson形式データを返す")
+    void getAirportTest1() throws Exception {
+        doReturn(new AirportEntity("CTS", "新千歳空港", "01", "北海道"))
+                .when(airportService)
+                .getAirport("CTS");
+
+        String actualResult = mockMvc
+                .perform(get("/airports/codes/CTS"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        String expectedResult = objectMapper.readTree(getJsonFileData("airport1.json")).toString();
+        JSONAssert.assertEquals(expectedResult, actualResult, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DisplayName("都道府県名から空港データを取得した場合、レスポンスコード200および該当する全ての空港のjson形式データを返す")
+    void getAirportsInPrefTest1() throws Exception {
+        doReturn(List.of(
+                new AirportEntity("CTS", "新千歳空港", "01", "北海道"),
+                new AirportEntity("HKD", "函館空港", "01", "北海道")))
+                .when(airportService)
+                .getAirportsByPref("北海道");
+
+        String actualResult = mockMvc
+                .perform(get("/airports/prefectures?prefName=北海道"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        String expectedResult = objectMapper.readTree(getJsonFileData("airport2.json")).toString();
+        JSONAssert.assertEquals(expectedResult, actualResult, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DisplayName("レスポンスコード200および全ての空港のjson形式データを返す")
+    void getAllAirportsTest1() throws Exception {
+        doReturn(List.of(
+                new AirportEntity("CTS", "新千歳空港", "01", "北海道"),
+                new AirportEntity("HKD", "函館空港", "01", "北海道"),
+                new AirportEntity("AOJ", "青森空港", "02", "青森県"),
+                new AirportEntity("HNA", "花巻空港", "03", "岩手県"),
+                new AirportEntity("SDJ", "仙台空港", "04", "宮城県")))
+                .when(airportService)
+                .getAllAirports();
+
+        String actualResult = mockMvc
+                .perform(get("/airports"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        String expectedResult = objectMapper.readTree(getJsonFileData("airport-all.json")).toString();
+        JSONAssert.assertEquals(expectedResult, actualResult, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DisplayName("空港データを追加した場合、レスポンスコード201および空港のjson形式データを返す")
+    void createAirportTest1() throws Exception {
+        doReturn(new AirportEntity("OKA", "那覇空港", "47", "沖縄県"))
+                .when(airportService)
+                .createAirport("OKA", "那覇空港", "47");
+
+        String actualResult = mockMvc
+                .perform(post("/airports")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new AirportRequestForm("OKA", "那覇空港", "47"))))
+                .andExpect(status().isCreated())
+                .andExpect(redirectedUrl("http://localhost/airports/OKA"))
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        String expectedResult = objectMapper.readTree(getJsonFileData("airport-create.json")).toString();
+        JSONAssert.assertEquals(expectedResult, actualResult, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DisplayName("空港データを更新した場合、レスポンスコード204を返す")
+    void updateAirportTest1() throws Exception {
+        doNothing().when(airportService).updateAirport("SDJ", "仙台国際空港", "04");
+
+        mockMvc.perform(patch("/airports/SDJ")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new AirportRequestForm("SDJ", "仙台国際空港", "04"))))
+                .andExpect(status().isNoContent())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        verify(airportService, times(1)).updateAirport("SDJ", "仙台国際空港", "04");
+    }
+
+    @Test
+    @DisplayName("空港データを削除した場合、レスポンスコード204を返す")
+    void deleteAirportTest1() throws Exception {
+        doNothing().when(airportService).deleteAirport("SDJ");
+
+        mockMvc.perform(delete("/airports/SDJ"))
+                .andExpect(status().isNoContent())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        verify(airportService, times(1)).deleteAirport("SDJ");
+    }
+*/
+}
---
+++ src/test/java/exercise/kadai10th/integration/PrefectureIntegrationTest.java
@@ -0,0 +1,177 @@
+package exercise.kadai10th.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exercise.kadai10th.controller.PrefectureController;
+import exercise.kadai10th.entity.PrefectureEntity;
+import exercise.kadai10th.requestform.PrefectureRequestForm;
+import exercise.kadai10th.service.PrefectureService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class PrefectureIntegrationTest {
+/*
+    @InjectMocks
+    private PrefectureController prefectureController;
+
+    @MockBean
+    PrefectureService prefectureService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    private final String BASE_PATH = "json-datasets/";
+
+    private String getJsonFileData(String fileName) throws IOException {
+        var jsonResult = resourceLoader.getResource("classpath:" + BASE_PATH + fileName);
+        return StreamUtils.copyToString(jsonResult.getInputStream(), StandardCharsets.UTF_8);
+    }
+
+    @Test
+    @DisplayName("都道府県コードから都道府県データを取得した場合、レスポンスコード200および都道府県のjson形式データを返す")
+    void getPrefByCodeTest1() throws Exception {
+        doReturn(new PrefectureEntity("01", "北海道"))
+                .when(prefectureService)
+                .getPrefByCode("01");
+
+        String actualResult = mockMvc
+                .perform(get("/prefectures/01"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        String expectedResult = objectMapper.readTree(getJsonFileData("prefecture1.json")).toString();
+        JSONAssert.assertEquals(expectedResult, actualResult, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DisplayName("都道府県名から都道府県データを取得した場合、レスポンスコード200および都道府県のjson形式データを返す")
+    void getPrefByNameTest1() throws Exception {
+        doReturn(new PrefectureEntity("02", "青森県"))
+                .when(prefectureService)
+                .getPrefByName("青森県");
+
+        String actualResult = mockMvc
+                .perform(get("/prefectures/names?prefName=青森県"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        String expectedResult = objectMapper.readTree(getJsonFileData("prefecture2.json")).toString();
+        JSONAssert.assertEquals(expectedResult, actualResult, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DisplayName("レスポンスコード200および登録済みの全ての都道府県のjson形式データを返す")
+    void getAllPrefsTest1() throws Exception {
+        doReturn(List.of(
+                new PrefectureEntity("01", "北海道"),
+                new PrefectureEntity("02", "青森県"),
+                new PrefectureEntity("03", "岩手県"),
+                new PrefectureEntity("04", "宮城県")))
+                .when(prefectureService)
+                .getAllPrefs();
+
+        String actualResult = mockMvc
+                .perform(get("/prefectures"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        String expectedResult = objectMapper.readTree(getJsonFileData("prefecture-all.json")).toString();
+        JSONAssert.assertEquals(expectedResult, actualResult, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DisplayName("都道府県データを追加した場合、レスポンスコード201および都道府県のjson形式データを返す")
+    void createPrefTest1() throws Exception {
+        doReturn(new PrefectureEntity("05", "秋田県"))
+                .when(prefectureService)
+                .createPref("05", "秋田県");
+
+        String actualResult = mockMvc
+                .perform(post("/prefectures")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new PrefectureRequestForm("05", "秋田県"))))
+                .andExpect(status().isCreated())
+                .andExpect(redirectedUrl("http://localhost/prefectures/05"))
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        String expectedResult = objectMapper.readTree(getJsonFileData("prefecture-create.json")).toString();
+        JSONAssert.assertEquals(expectedResult, actualResult, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DisplayName("都道府県データを更新した場合、レスポンスコード204を返す")
+    void updatePrefTest1() throws Exception {
+        doNothing().when(prefectureService).updatePref("02", "あおもりけん");
+
+        mockMvc.perform(patch("/prefectures/02")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new PrefectureRequestForm("02", "あおもりけん"))))
+                .andExpect(status().isNoContent())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        verify(prefectureService, times(1)).updatePref("02", "あおもりけん");
+    }
+
+    @Test
+    @DisplayName("都道府県データを削除した場合、レスポンスコード204を返す")
+    void deletePrefTest1() throws Exception {
+        doNothing().when(prefectureService).deletePref("11");
+
+        mockMvc.perform(delete("/prefectures/11"))
+                .andExpect(status().isNoContent())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        verify(prefectureService, times(1)).deletePref("11");
+    }
+
+*/
+}
